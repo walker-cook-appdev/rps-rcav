3f7e55630f08f19cefb5bc620db6f96f7e43442b
walker-cook <jwcook99@gmail.com>
2022-01-26 14:50:48 +0000

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock 
+    #redirect_to("https://wikipedia.com")
+    #render({:plain=>"howdy world"})
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
 
 end
